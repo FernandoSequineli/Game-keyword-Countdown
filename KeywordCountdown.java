65279749e2a27a0ac1e2bdc2b57096139cb105d9
Fernando Sequineli <56481201+FernandoSequineli@users.noreply.github.com>
2021-02-27 10:10:04 +0000

    Update KeywordCountdown.java

--- KeywordCountdown.java
+++ KeywordCountdown.java
@@ -1,14 +1,3 @@
-/*
- *This program is similar to the tv show countdown, there are two players and they can only enter Java keywords.
- *the player chooses how many rounds he wants to play, in each turn the player chooses how many vowels and consonants  
- * between 9 letters and both need to form a word, the one who assembles the largest word wins.
- */
-
-/**
- * KeywordCountdown.java
- * @author Fernando Sequineli
- * 22th April 2020
- */
 
 
 //all imported class in the program
